7d507918df5a4da51444260000d37a81c073a72b
barcai <udvardy.zsombor@gmail.com>
2016-01-29 20:34:05 +0000

    JumpingP prototype 3

--- Scripts/JumpyPlatform.cs
+++ Scripts/JumpyPlatform.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class JumpyPlatform : MonoBehaviour {
 
 	public float extra_jump; // number of extra jumps when player arrives on the platform
+	public float xmin = 50;
+	public float xmax = 500;
+	public float ymin = 50;
+	public float ymax = 300;
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.tag == "Player") {
 			Rigidbody2D rb = coll.gameObject.GetComponent<Rigidbody2D>();
-			rb.AddForce(new Vector2(Random.Range(10, 50), Random.Range(10, 100)));
+			rb.AddForce(new Vector2(Random.Range(xmin, xmax), Random.Range(ymin, ymax)));
 			extra_jump--;
 		}
 	}
