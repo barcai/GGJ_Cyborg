9d81c0372f5b075c339bc397ee84320e31def081
barcai <udvardy.zsombor@gmail.com>
2016-01-29 20:25:32 +0000

    Basic platfrom with Jumpy properties prototype

---
+++ Scripts/JumpyPlatform.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpyPlatform : MonoBehaviour {
+
+	public float extra_jump; // number of extra jumps when player arrives on the platform
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			RigidBody2D rb = coll.GetComponent<RigidBody2D>();
+			while (extra_jump > 0) {
+				rb.AddForce(new Vector2(Random.Range(10, 50), Random.Range(10, 100)));
+				extra_jump--;
+            }
+		}
+	}
+}
