c78a02ef5df6a613819b0b2d3b34eb94e46f2985
LendelTheGreat <langer.tomas@yahoo.com>
2016-01-30 18:15:40 +0000

    Random trees spawn

---
+++ Scripts/ScrollingTrees.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingTrees : MonoBehaviour {
+
+    public float speed = 0.005f;
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            Vector3 offset = new Vector3(-speed * Input.GetAxis("Horizontal"), 0, 0);
+            transform.position = transform.position + offset;
+        }
+    }
+
+}
---
+++ Scripts/SpawnTrees.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnTrees : MonoBehaviour {
+
+    public Vector2 range = new Vector2(0f, 0f);
+    public Vector2 density = new Vector2(1f, 1f);
+
+    public GameObject[] prefabs;
+
+    void Start () {
+        float posX = range.x;
+
+        while (posX <= range.y)
+        {
+            // Spawn tree at pos
+            GameObject prefab = prefabs[Random.Range(0, prefabs.Length)];
+
+            GameObject instance = GameObject.Instantiate(prefab);
+            instance.transform.parent = this.transform;
+            float posY = instance.transform.position.y + instance.GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2f - 0.9f + Random.Range(0, 0.3f);
+            instance.transform.position = new Vector3(posX, posY, instance.transform.position.z);
+
+
+            posX += Random.Range(density.x, density.y);
+        }
+	}
+}
