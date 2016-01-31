94790c54076641e2d006e20c22dbe0dc7d08c29a
barcai <udvardy.zsombor@gmail.com>
2016-01-31 13:10:03 +0000

    Running animation stuff

--- Scripts/AnimationController.cs
+++ Scripts/AnimationController.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class AnimationController : MonoBehaviour {
 
 
-    public Animator animator;
+    Animator animator;
 
     private PlayerController controller;
+    private Rigidbody2D rb;
 
 
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
         controller = GetComponent<PlayerController>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,28 @@ public class AnimationController : MonoBehaviour {
         {
             animator.SetBool("JumpButtonPressed", true);
         }
-        Debug.Log(animator.GetBool("JumpButtonPressed"));
+
+        if (rb.velocity.x > 0.5)
+        {
+            if (transform.rotation.y != 0)
+            {
+                transform.Rotate(new Vector3(0, -180, 0));
+            }
+            animator.SetBool("Moving", true);
+        } else if (rb.velocity.x < -0.5)
+        {
+            if (transform.rotation.y == 0)
+            {
+                transform.Rotate(new Vector3(0, 180, 0));
+            }
+            animator.SetBool("Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+
+        Debug.Log(animator.GetBool("JumpButtonPressed") + " " + animator.GetBool("Moving"));
     }
 
     void OnCollisionEnter2D(Collision2D obj)
