e8a1720e7a2fed48664d1a43f0abd59396c7883b
LendelTheGreat <langer.tomas@yahoo.com>
2016-01-31 13:01:17 +0000

    climbing implemented

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -6,15 +6,19 @@ public class PlayerController : MonoBehaviour {
 	public float speed = 100;
     public Vector2 speedLimit = new Vector2(3, 5);
 	public float jumpHeight = 100;
-	public bool canControl = true;
-	public bool onGround = true;
+    public float climbPower = 10;
+    public float climbLimit = 50;
+    public bool canControl = true;
+    public bool canClimb = true;
+    public bool nearClimableObject = false;
+    public bool onGround = true;
 	public int playerState = 0; // 0 normal, 1 arms_done, 2 legs_done, 3 lungs_done
 
 	public GameObject mCamera;
 
 	private Rigidbody2D rb;
 
-	void Start()
+    void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
 	}
@@ -35,9 +39,17 @@ public class PlayerController : MonoBehaviour {
 				rb.AddForce (new Vector2 (0, jumpHeight));
 				onGround = false;
 			}
+            else if (nearClimableObject && Input.GetButton("Jump")) // Climbing
+            {
+                if (rb.velocity.y < climbLimit)
+                {
+                    rb.AddForce(new Vector2(0, climbPower));
+                    nearClimableObject = false;
+                }
+            }
 
-			// Braking
-			if (Mathf.Abs (Input.GetAxis ("Horizontal")) < 0.5) {
+            // Braking
+            if (Mathf.Abs (Input.GetAxis ("Horizontal")) < 0.5) {
 				rb.velocity = new Vector2 (rb.velocity.x * 1 / 2, rb.velocity.y);
 			}
 	
@@ -71,4 +83,12 @@ public class PlayerController : MonoBehaviour {
             onGround = true;
 		}
 	}
+
+    void OnCollisionStay2D(Collision2D obj)
+    {
+        if (canClimb && obj.gameObject.tag == "Ground")
+        {
+            nearClimableObject = true;
+        }
+    }
 }
